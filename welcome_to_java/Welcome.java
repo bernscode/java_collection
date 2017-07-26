6bc06d39b97842fcdc2e82aead4b6e180bf9b4d2
bernscode <bern@bernscode.com>
2017-07-25 23:17:48 -0700

    Added welcome_to_java

---
+++ welcome_to_java/Welcome.java
@@ -0,0 +1,11 @@
+/*WelcomebErNbleNd.java   Author: bErNbLeNd
+Text-printing program.*/
+
+public class Welcome
+{
+   //main method begins execution of Java application
+   public static void main(String[] args)
+   {
+      System.out.println("Welcome to Java Programming bern!");
+   }//end method main
+}//end class WelcomebErNbLeNd
